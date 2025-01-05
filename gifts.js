50e7eecd83a8286e464e15091776d13c54e40e79
AnnaKRP <98829629+AnnaKRP@users.noreply.github.com>
2025-01-05 23:11:36 +0000

    adding more functionalities

--- gifts.js
+++ gifts.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('#tabs a');
+    const cardContainer = document.getElementById('card-container');
     const burgerMenu = document.getElementById('burger-menu');
     const menu = document.getElementById('menu');
-    const menuLinks = menu ? menu.querySelectorAll('a') : [];
+    const menuLinks = menu.querySelectorAll('a');
+    const arrowUp = document.getElementById('arrow-up');
     const modal = document.getElementById('gift-modal');
     const modalOverlay = document.getElementById('modal-overlay');
     const closeModalBtn = document.getElementById('close-modal');
@@ -10,143 +13,86 @@ document.addEventListener('DOMContentLoaded', () => {
     const modalDescription = document.getElementById('modal-description');
     const modalSuperpowers = document.getElementById('modal-superpowers');
 
-    // BURGER MENU
-    if (burgerMenu && menu) {
-        burgerMenu.addEventListener('click', () => {
-            console.log('Burger menu clicked');
-            menu.classList.toggle('active');
-            burgerMenu.classList.toggle('open');
-            document.body.classList.toggle('no-scroll');
+    // toggle the active class on the menu and burger icon when the burger menu is clicked
+    burgerMenu.addEventListener('click', () => {
+        console.log('Burger menu clicked');
+        menu.classList.toggle('active');
+        burgerMenu.classList.toggle('open');
+        document.body.classList.toggle('no-scroll');
+    });
+
+    // remove active class when any menu link is clicked & closing the menu
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('active');
+            burgerMenu.classList.remove('open');
+            document.body.classList.remove('no-scroll');
         });
+    });
 
-        menuLinks.forEach(link => {
-            link.addEventListener('click', () => {
-                menu.classList.remove('active');
-                burgerMenu.classList.remove('open');
-                document.body.classList.remove('no-scroll');
-            });
-        });
-    }
+    // ARROW UP
+    arrowUp.classList.remove('visible');
 
-    // SLIDER
-    const leftButton = document.getElementById('left-button');
-    const rightButton = document.getElementById('right-button');
-    const rowSlider = document.getElementById('row-slider');
-
-    let sliderPosition = 0;
-
-    // get the screen width and set the number of clicks needed
-    function getSliderClicks() {
-        const screenWidth = window.innerWidth;
-
-        if (screenWidth >= 769) {
-            return 3;
-        } else if (screenWidth >= 380) {
-            return 6;
+    // show the "arrow-up" button when scrolling down to 300, and hide it when scrolling up
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            arrowUp.classList.add('visible');
         } else {
-            return 9;
+            arrowUp.classList.remove('visible');
         }
-    }
+    });
 
-    // calculate how far the slider should move with each click
-    function calculateMovement() {
-        const totalWidth = 940;
-        const visibleWidth = 400;
-        const clicks = getSliderClicks();
-        return (totalWidth - visibleWidth) / clicks;
-    }
-
-    // move the slider left or right
-    function moveSlider(direction) {
-        const movement = calculateMovement();
-
-        // calculate the new slider position
-        if (direction === 'right') {
-            sliderPosition += movement;
-        } else if (direction === 'left') {
-            sliderPosition -= movement;
-        }
-
-        // set the new position of the slider
-        rowSlider.style.transform = `translateX(-${sliderPosition}px)`;
-
-        // disable the left button if at the extreme left position
-        leftButton.disabled = sliderPosition <= 0;
-
-        // disable the right button if at the extreme right position
-        const maxPosition = (940 - 400);
-        rightButton.disabled = sliderPosition >= maxPosition;
-    }
-
-    // add event listeners to the buttons
-    if (rightButton && leftButton) {
-        rightButton.addEventListener('click', () => {
-            moveSlider('right');
+    // scroll to the top of the page when the "arrow-up" button is clicked
+    arrowUp.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
         });
+    });
 
-        leftButton.addEventListener('click', () => {
-            moveSlider('left');
-        });
-
-        // set buttons to the starting position
-        moveSlider('right');
-    }
-
-    // TIMER
-    function updateTimer() {
-        const currentTime = new Date();
-        const targetDate = new Date('December 31, 2025 00:00:00 UTC');
-        const timeRemaining = targetDate - currentTime;
-
-        if (timeRemaining <= 0) {
-            clearInterval(timerInterval);
-            const par = document.getElementById('par');
-            const timer = document.getElementById('timer');
-            if (par && timer) {
-                par.textContent = "Happy New Year!";
-                timer.classList.add('hidden');
-            }
-            return;
-        }
-
-        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-        const daysElement = document.getElementById('days');
-        const hoursElement = document.getElementById('hours');
-        const minsElement = document.getElementById('mins');
-        const secondsElement = document.getElementById('seconds');
-
-        if (daysElement && hoursElement && minsElement && secondsElement) {
-            daysElement.querySelector('h2').textContent = String(days).padStart(2, '0');
-            hoursElement.querySelector('h2').textContent = String(hours).padStart(2, '0');
-            minsElement.querySelector('h2').textContent = String(minutes).padStart(2, '0');
-            secondsElement.querySelector('h2').textContent = String(seconds).padStart(2, '0');
-        }
-    }
-
-    const timerInterval = setInterval(updateTimer, 1000);
-    updateTimer();
-
-    // fetch and display GIFTS from gifts.json file
+    // fetch data from the JSON file
     fetch('./gifts.json')
         .then(response => response.json())
-        .then(data => displayRandomGifts(data))
+        .then(data => {
+            // display all gifts
+            displayGifts(data);
+
+            // Ffilter gifts by category
+            tabs.forEach(tab => {
+                tab.addEventListener('click', (event) => {
+                    event.preventDefault();
+
+                    // remove active class from all tabs
+                    tabs.forEach(tab => tab.classList.remove('active'));
+
+                    // add active class to clicked tab
+                    tab.classList.add('active');
+
+                    // filter gifts based on the clicked tab
+                    const category = tab.textContent.trim().toLowerCase();
+
+                    if (category === 'all') {
+                        displayGifts(data);
+                    } else {
+                        const filteredGifts = data.filter(gift => gift.category.toLowerCase() === category);
+                        displayGifts(filteredGifts);
+                    }
+                });
+            });
+        })
         .catch(error => console.error("Error loading JSON data:", error));
 
-    // display 4 random gifts
-    function displayRandomGifts(gifts) {
-        const cardContainer = document.getElementById('card-container');
-        if (!cardContainer) return;
+    // function to display gifts in the card container
+    function displayGifts(gifts) {
+        cardContainer.innerHTML = '';
 
-        const shuffledGifts = shuffleArray(gifts).slice(0, 4);
-
-        shuffledGifts.forEach(gift => {
+        // go through the gifts and create a card for each one
+        gifts.forEach(gift => {
             const card = document.createElement('div');
             card.classList.add('card', 'flex-column');
 
+            // set category color and image based on gift category
             let categoryColor = '';
             let categoryImage = '';
 
@@ -168,6 +114,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     break;
             }
 
+            // add card content
             card.innerHTML = `
                 <div class="card-img ${categoryImage}"></div>
                 <div class="card-name flex-column">
@@ -176,19 +123,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
             `;
 
+            // add event listener to open the modal when the card is clicked
             card.addEventListener('click', () => openModal(gift));
 
+            // add card to container
             cardContainer.appendChild(card);
         });
-    }
-
-    // shuffle function
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
     }
 
     // MODAL
